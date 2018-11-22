7ced94e98d5aeedcf38d519590a4002566a42842
TheNoggie <33137826+TheNoggie@users.noreply.github.com>
2018-11-22 14:49:13 +0100

    Create ifj18.c

---
+++ ifj18.c
@@ -0,0 +1,13 @@
+/**
+ * Project: FIT VUT Brno IFJ Project
+ * Description: Implementation of Compiler for IFJ18 language.
+ * Author: Vojtech Pilar (xpilar06)
+ * Contact: xpilar06@stud.fit.vutbr.cz
+ * Date of creation: 2018-11-20
+ * Version: 1.00
+ * */
+ 
+ int main(){
+ 
+     return 0;
+ }
